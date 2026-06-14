07ae5ed0350851164bdcdf5639bdac53eaf57f95
Peidi Li <peidili@peidis-air-2.home>
2026-06-14 20:12:16 +0100

    Update Python practice

--- python-practice/code.py
+++ python-practice/code.py
@@ -9,6 +9,20 @@
 # favourite_colour = input("What is your favourite colour? ")
 # print(f"{name}, your favourite colour is {favourite_colour}.")
 
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
-print(f"{num1} + {num2} = {num1 + num2}")
+# num1 = int(input("Enter your first number: "))
+# num2 = int(input("Enter your second number: "))
+# print(f"{num1} + {num2} = {num1 + num2}")
+
+# number = int(input("Please enter a number: "))
+# if number % 2 == 0:
+#     print(f"{number} is an even number.")
+# else:
+#     print(f"{number} is an odd number.")
+
+# number = int(input("Please enter a number: "))
+# if number > 0:
+#     print(f"{number} is a positive number.")
+# elif number < 0:
+#     print(f"{number} is a negative number.")
+# else:
+#     print(f"{number} is zero.")
