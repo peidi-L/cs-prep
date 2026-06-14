03bf4fb805b808aae98b048d1e1f16623260570e
Peidi Li <peidili@peidis-air-2.home>
2026-06-14 15:44:55 +0100

    Updated practice exercises

--- python-practice/code.py
+++ python-practice/code.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+# print("Hello, World!")
+# name = input("What is your name? ")
+# print(f"Nice to meet you, {name}!")
+
+# age = input("How old are you? ")
+# future_age = int(age) + 5
+# print(f"In 5 years, you will be {future_age} years old.")
+
+# favourite_colour = input("What is your favourite colour? ")
+# print(f"{name}, your favourite colour is {favourite_colour}.")
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+print(f"{num1} + {num2} = {num1 + num2}")
