61b9167b94084291e52c25ac9293a0509f24311a
Peidi Li <peidili@peidis-air-2.home>
2026-06-19 00:35:43 +0100

    Add divisible by 3 and 5 practice

--- python-practice/code.py
+++ python-practice/code.py
@@ -51,3 +51,9 @@
 # else:
 #     print(f"{number} is not divisible by 3.")
     
+number = int(input("Please enter a number: "))
+if number % 3 == 0 and number % 5 == 0:
+    print(f"{number} is divisible by both 3 and 5.")
+else:
+    print(f"{number} is not divisible by both 3 and 5.")
+    
