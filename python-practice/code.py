a6908dc6485a9c0eeadabe5be9de55accb6753af
Peidi Li <peidili@peidis-air-2.home>
2026-06-19 09:31:16 +0100

    Added testing for largest number

--- python-practice/code.py
+++ python-practice/code.py
@@ -62,3 +62,16 @@
 #     print("Access granted.")
 # else:
 #     print("Access denied.")
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+num3 = int(input("Please enter your third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3  
+
+print(f"The largest number is: {largest}")
