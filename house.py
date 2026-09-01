7c949690c1ecfbc2ce5e849839a363f74913f56e
peidi-L <peidi-L@users.noreply.github.com>
2026-09-01 21:36:40 +0800

    Update house program

--- house.py
+++ house.py
@@ -7,12 +7,23 @@
 
 name = input("What's your name? ")
 
+# match name:
+#     case "Harry":
+#         print("Gryffindor")
+#     case "Hermione":
+#         print("Gryffindor")
+#     case "Ron":
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin")
+#     case _:
+#         print("Who?")
+
 match name:
-    case "Harry":
-        print("Gryffindor")
-    case "Hermione":
-        print("Gryffindor")
-    case "Ron":
+    case "Harry" | "Hermione" | "Ron":
         print("Gryffindor")
     case "Draco":
         print("Slytherin")
+    case _:
+        print("Who?")
+
